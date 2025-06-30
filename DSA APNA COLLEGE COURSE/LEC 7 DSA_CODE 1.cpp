644991f4f7f84ce5098ecde82d95b4a2c66be24b
Vishal Pramanik <vishal.pramanik@outlook.com>
2025-06-30 19:51:06 +0530

    Updated ALL

---
+++ DSA APNA COLLEGE COURSE/LEC 7 DSA_CODE 1.cpp
@@ -0,0 +1,12 @@
+//LEC 7 DSA_25-06-2025_DATA MODIFIERS
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << sizeof(int) << "\n";
+    cout << sizeof(long int) << "\n";
+    cout << sizeof(short int) << "\n";
+    cout << sizeof(long long int) << "\n";
+    return 0;
+}
