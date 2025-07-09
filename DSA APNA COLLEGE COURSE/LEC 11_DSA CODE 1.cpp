7dc3987ab52e33b81c8ac79053fc1c162f1fdf38
Vishal Pramanik <vishal.pramanik@outlook.com>
2025-07-09 17:02:07 +0530

    COMPETED PAIR SUM PROBLEM

--- DSA APNA COLLEGE COURSE/LEC 11_DSA CODE 1.cpp
+++ DSA APNA COLLEGE COURSE/LEC 11_DSA CODE 1.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 // Function for pair sum : Returns the index of the pair from the array having sum equal to target
-void pairSum(vector<int> vec, int target) {
+void pairSum1(vector<int> &vec, int target) {
     // Loop for checking pair sum
     for (int i=0; i<vec.size(); i++) {
 
@@ -25,12 +25,41 @@ void pairSum(vector<int> vec, int target) {
     cout << "Pair not found\n";
     return;
 }
+vector<int> pairSum2(vector<int> &vec, int target) {
+    int start = 0;
+    int end = vec.size()-1;
+
+    while (start < end) {
+        int currSum = vec[start] + vec[end];
+
+        if (currSum == target) {
+            return {start, end};
+        } else if (currSum < target) {
+            start++;
+        } else {
+            end--;
+        }
+
+    }
+
+    return {};
+}
 
 int main() {
     vector<int> vec1 = {2,7,11,15};
     int target = 9;
 
-    pairSum(vec1, target);
+    pairSum1(vec1, target);
+    cout << "\n";
+
+    vector<int> ans = pairSum2(vec1, target);
+
+    cout << "Index of vector pair having sum equal to target(" << target << ") = "<<
+        ans[0] << " and " << ans[1] << "\n" << "Value of vector pair = " <<
+            vec1[ans[0]] << " and " << vec1[ans[1]] << "\n" << "Justification : " <<
+                vec1[ans[0]] << " + " << vec1[ans[1]] << " = " << target << "\n";
+
+    // cout << "Value of vector pair : " << vec1[ans[0]] << " and " << vec1[ans[1]] << "\n";
 
     return 0;
 }
